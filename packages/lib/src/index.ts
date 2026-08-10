83b3be943a747b681abdff2997124443459f4b3c
Dietrich <dietrich@dietrich-development.de>
2026-08-11 00:13:34 +0200

    feat(lib): add vec3 math utilities

--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,1 +1,17 @@
+export {
+  add,
+  cross,
+  distance,
+  distanceSq,
+  dot,
+  equalsApprox,
+  length,
+  lengthSq,
+  lerp,
+  normalize,
+  scale,
+  sub,
+  type Vec3,
+  vec3,
+} from "./math/vec3"
 export { err, ok, type Result } from "./result"
